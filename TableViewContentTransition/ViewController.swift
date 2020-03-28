830526b6ec61b7982f555dafc1333b8a5fdd2987
funnything <coding.while.sleep@gmail.com>
2020-03-28 23:36:57 +0900

    Control buttons

--- TableViewContentTransition/ViewController.swift
+++ TableViewContentTransition/ViewController.swift
@@ -7,11 +7,12 @@
 //
 
 import Cartography
+import Signals
 import Then
 import UIKit
 
 class ViewController: UIViewController {
-    var content = [0, 1, 2, 3, 4]
+    var content = [0, 1, 2, 3]
 
     lazy var emptyView: UIView = undefined()
     lazy var tableView: UITableView = undefined()
@@ -33,10 +34,55 @@ class ViewController: UIViewController {
             view.addSubview($0)
             constrain($0) { $0.matchParent() }
         }
+
+        let sv = vstack(spacing: 16).then {
+            view.addSubview($0)
+            constrain($0) { v in
+                v.bottom == v.sv.safeAreaLayoutGuide.bottom
+                v.leading == v.sv.leading
+                v.trailing == v.sv.trailing
+            }
+        }
+
+        let rows = 4
+        let columns = 4
+        (0..<rows).forEach { y in
+            _ = hstack().then { row in
+                row.distribution = .fillEqually
+                sv.addArrangedSubview(row)
+
+                (0..<columns).forEach { x in
+                    _ = UIButton(type: .system).then {
+                        let seq = toSequence(y * columns + x)
+                        $0.onTouchUpInside.subscribe(with: self) { [unowned self] in
+                            self.content = seq
+                            self.tableView.reloadData()
+                        }
+                        let title = seq.isEmpty ? "empty" : seq.map { "\($0)" }.joined(separator: "-")
+                        $0.setTitle(title, for: .normal)
+                        row.addArrangedSubview($0)
+                    }
+                }
+            }
+        }
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+
+    func toSequence(_ value: Int) -> [Int] {
+        var v = value
+        var i = 0
+        var seq = [Int]()
+        while v > 0 {
+            if v % 2 > 0 {
+                seq.append(i)
+            }
+            v /= 2
+            i += 1
+        }
+        return seq
     }
 }
 
@@ -70,5 +116,23 @@ extension ViewProxy {
 
     func matchParent() {
         match(to: sv)
+    }
+}
+
+func hstack(spacing: CGFloat? = nil) -> UIStackView {
+    UIStackView().then {
+        $0.axis = .horizontal
+        if let spacing = spacing {
+            $0.spacing = spacing
+        }
+    }
+}
+
+func vstack(spacing: CGFloat? = nil) -> UIStackView {
+    UIStackView().then {
+        $0.axis = .vertical
+        if let spacing = spacing {
+            $0.spacing = spacing
+        }
     }
 }
